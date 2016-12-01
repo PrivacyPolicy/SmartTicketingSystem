660a5a19683bb6dfbe7d431fdbc0990c5eedd90d
criggs626 <criggs@dnc.io>
2016-12-01 13:55:13 -0500

    added more code

--- sql/create.sql
+++ sql/create.sql
@@ -1,9 +1,13 @@
-CREATE TABLE events(
+create database ticketing_test;
+
+use ticketing_test;
+
+CREATE TABLE tickets(
 id int NOT NULL AUTO_INCREMENT,
 subject varchar(1000),
 user_id varchar(100),
 asignee_id varchar(100),
 priority int(1),
-description varchar(20000)
+description varchar(20000),
 PRIMARY KEY (id)
 );
